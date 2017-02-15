38ae31d1f805f821793bd19c36703aad0d825b15
Jason Li <jasonli2310@gmail.com>
2017-02-15 10:52:05 -0600

    directors done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,16 +1,17 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
     @director = Director.find(params[:id])
   end
 
-  def new
+  def new_form
   end
 
   def create_row
+
     @director = Director.new
     @director.dob = params[:dob]
     @director.name = params[:name]
@@ -29,10 +30,10 @@ class DirectorsController < ApplicationController
   def update_row
     @director = Director.find(params[:id])
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
     @director.save
 
